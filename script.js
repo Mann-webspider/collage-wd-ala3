1b2d5752faadfa02a13aeae0e381902ff38e09a1
Mann-webspider <maandalsania11@gmail.com>
2023-08-23 17:19:01 +0530

    Added model functionality

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let plus = document.getElementById("plus")
 let minus = document.getElementById("minusbtn")
 let quantity = document.getElementById("product-range")
 let bigImg = document.getElementById("bigImg")
+let bigImgModel = document.getElementById("bigImgModel")
 let thumbnail = document.querySelectorAll(".product-thumb button img")
+let thumbnailModel = document.querySelectorAll(".product-thumb-model button img")
 let addCart = document.getElementById("add-cart")
 let productPrice = document.querySelector(".item-price div h2") 
 let checkoutBill = document.getElementById("checkout-bill")
@@ -36,6 +38,18 @@ for(btn of thumbnail){
         
         let modify = img.replace("-thumbnail","")
         bigImg.src = modify
+      
+
+    })
+}
+for(btn of thumbnailModel){
+    btn.addEventListener("click",function (){
+        let img = this.src
+        
+        let modify = img.replace("-thumbnail","")
+        
+        bigImgModel.src = modify
+
     })
 }
 
@@ -58,3 +72,31 @@ deleteCart.addEventListener("click",()=>{
     nonEmptyCart.classList.add("hidden")
 
 })
+
+
+// code for opening model
+// Get the modal
+var modal = document.getElementById("myModel");
+
+// Get the button that opens the modal
+var btn = document.getElementById("openModel");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
